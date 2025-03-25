9a7bd5896d8313b3e24703896ea4109959e1a921
Izuna <Izuna@GIGABYTE_Z690-X>
2025-03-25 12:20:56 +0100

    added visualization

---
+++ moduł1.py
@@ -0,0 +1,40 @@
+import pandas
+import numpy
+import matplotlib.pyplot as plt
+
+#this was done once for dataset manipulation to achive redable data
+
+#Data = pandas.read_csv("Iraq_dataset.csv")
+#Data['Gender'] = Data['Gender'].replace({'F': 0, 'M': 1})
+#Data['CLASS'] = Data['CLASS'].replace({'N': 0, 'P': 1, 'Y': 2})
+#print(Data['CLASS'])
+#Data.to_csv('Modified_Iraq_dataset.csv',index = False)
+
+#all data types are alright, this dataset is fine:
+
+Data = pandas.read_csv("Modified_Iraq_dataset.csv")
+Data.drop(['ID','No_Pation'], axis = 1, inplace = True)
+
+#since 1 is the smallest group now we randomly choose 52 samples from 'CLASS' groups 0, ,2 so we have equal samples for all states:
+
+Data_0 = Data[Data['CLASS'] == 0].sample(n=53, ignore_index= False)
+#Data_0.info()
+#print(Data_0)
+Data_1 = Data[Data['CLASS'] == 1]
+#print(Data_1)
+Data_2 = Data[Data['CLASS'] == 2].sample(n=53, ignore_index= False)
+#print(Data_2)
+#print(Data_0.size,Data_1.size,Data_2.size)
+#print(Data_0,Data_1,Data_2)
+
+# now we plot histograms for chosen data groups to see corelations and so on
+
+plt.hist(Data_0['BMI'], label= 'Healthy', bins= 12, alpha= 0.7)
+plt.hist(Data_1['BMI'], label= 'Maybe', bins= 12, alpha= 0.7)
+plt.hist(Data_2['BMI'], label= 'Diabetes', bins= 12, alpha= 0.7)
+
+plt.xlabel("BMI")
+plt.ylabel("Patient")
+
+plt.legend()
+plt.show()
